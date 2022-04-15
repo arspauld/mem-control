5e312daf50189f798638f753539b912f87917deb
Will-McCormick <wrm0006@uah.edu>
2022-04-15 16:51:44 -0500

    New  always @sw\ blocks

--- src/InOutControl.sv
+++ src/InOutControl.sv
@@ -9,7 +9,7 @@ module InOutControl(
     output [15:0] ioDataOut,//Data sent to the MemoryControl from InputOutputControl during a write command 
     output [24:0] memAddrOut,//Address sent to the MemoryControl from InputOutputControl 
     output ioCmdDoneOut,     //Flag sent form InputOutputControl to the MemoryControl to singal wheter it is done with getting input or not
-    output [31:0] dispData //This will be sent to a display module to be displayed on the LEDs idk exactly how these work so I just figured it would be 32-bits (8 bits per LED)..
+    output [15:0] dispData //This will be sent to a display module to be displayed on the LEDs idk exactly how these work so I just figured it would be 32-bits (8 bits per LED)..
 );
 
 logic [24:0] nextAddr;
@@ -18,7 +18,7 @@ logic [1:0] currentCmd;
 logic [3:0] swState;
 
 logic localDone;
-//logic canChangeCmd;
+//logic //canChangeCmdmd;
 logic localMemCmd;
 logic localIoCmdDoneOut;
 logic [15:0] localIoDataOut;
@@ -28,7 +28,7 @@ logic [24:0] localMemAddrOut;
 // initial begin
 //     nextAddr = 32'h00000000;
 //     localDone = 1'b0;
-//     //canChangeCmd = 1'b1;
+//     //canChangeCmdmd = 1'b1;
 //     localMemAddrOut = 32'h00000000;
 //     localIoDataOut = 16'h0000;
 // end
@@ -38,7 +38,7 @@ always@(posedge memCmdDoneIn) begin
     localIoDataOut <= 16'h0000;
     localIoCmdDoneOut <= 1'b0;
     if(cmdLvl != 2'b00) begin
-        //canChangeCmd <= 1'b1;
+        //canChangeCmdmd <= 1'b1;
     end
     
 end
@@ -50,8 +50,8 @@ always @(posedge clk) begin         //ALWAYS @ POSEDGE CLK
     // if(currentCmd === 2'bxx) begin
     //     currentCmd <= 2'b00;
     // end
-    // if(//canChangeCmd === 1'bx) begin
-    //     //canChangeCmd <= 1'b1;
+    // if(//canChangeCmdmd === 1'bx) begin
+    //     //canChangeCmdmd <= 1'b1;
     // end 
     if(memCmdDoneIn == 1'b1) begin
         case(button) 
@@ -62,12 +62,12 @@ always @(posedge clk) begin         //ALWAYS @ POSEDGE CLK
                     localIoCmdDoneOut <= 1'b0;
                     localMemCmd <= 2'b11;
                     currentCmd <= 2'b00; //Sets the current command to clear
-                    //canChangeCmd <= 1'b1;
+                    //canChangeCmdmd <= 1'b1;
             end
             2'b10 : begin //EXECUTE CMD
                 if(currentCmd == 2'b00) begin //Initiate Clear
                     cmdLvl <= cmdLvl + 1;
-                    //canChangeCmd <= 1'b0;
+                    //canChangeCmdmd <= 1'b0;
                 end
                 else if(currentCmd == 2'b01) begin //Initiate Write
                     if(cmdLvl == 2'b11) begin
@@ -76,29 +76,29 @@ always @(posedge clk) begin         //ALWAYS @ POSEDGE CLK
                         localIoCmdDoneOut <= 1'b1;
                     end
                     cmdLvl <= cmdLvl + 1;
-                    //canChangeCmd <= 1'b0;
+                    //canChangeCmdmd <= 1'b0;
 
                 end
                 else if(currentCmd == 2'b10) begin//Initiate Read
                     cmdLvl <= cmdLvl + 1;
-                    //canChangeCmd <= 1'b0;
+                    //canChangeCmdmd <= 1'b0;
                 end
                 // else begin    //DEFAULT CMD RESET
                 //     cmdLvl <= 2'b00; //Sets the level of the command to 0 (i.e. select instruction level)
                 //     localMemCmd <= 2'b11;
                 //     currentCmd <= 2'b00; //Sets the current command to clear
-                //     //canChangeCmd <= 1'b1;
+                //     //canChangeCmdmd <= 1'b1;
                 // end
             end
             2'b01: begin    //CYCLE TO NEXT CMD
-//                if(//canChangeCmd == 2'b1) begin
+                //if(//canChangeCmd == 2'b1) begin
                     if(currentCmd == 2'b10 || currentCmd == 2'b11) begin
                         currentCmd <= 2'b00;
                     end 
                     else begin
                         currentCmd <= currentCmd + 1;
                     end
-                end
+                // end
             end
             default : begin
             end
@@ -130,13 +130,12 @@ always @(posedge clk) begin         //ALWAYS @ POSEDGE CLK
                     localIoDataOut <= 16'h0000;
                     localMemCmd <= 2'b11;
                     currentCmd <= 2'b00;
-                    //canChangeCmd <= 1'b1;
+                    //canChangeCmdmd <= 1'b1;
                 end
             endcase
         end
     end
 end
-
 always @(posedge sw[0])begin    //INCREMENTOR FOR LED 0
     if(memCmdDoneIn == 1'b1) begin
         case(currentCmd)
@@ -198,6 +197,7 @@ always @(posedge sw[0])begin    //INCREMENTOR FOR LED 0
     end
 end
 
+
 always @(posedge sw[1])begin    //INCREMENTOR FOR LED 0
     if(memCmdDoneIn == 1'b1) begin
         case(currentCmd)
@@ -258,7 +258,6 @@ always @(posedge sw[1])begin    //INCREMENTOR FOR LED 0
         endcase
     end
 end
-
 always @(posedge sw[2])begin    //INCREMENTOR FOR LED 0
     if(memCmdDoneIn == 1'b1) begin
         case(currentCmd)
@@ -376,3 +375,125 @@ end
     assign ioCmdDoneOut = localIoCmdDoneOut;
 
 endmodule
+// always @(posedge sw[0]) begin
+//     case(state)
+//         (WRITE_LEVEL1 || READ_LEVEL1) : begin
+//               if(localmemoryAddress[3:0] == 4'hF) begin
+//                     localMemoryAddress[3:0] <= 4'h0;
+//                 end
+//                 else begin
+//                     localMemoryAddress[3:0]++;
+//                 end
+//             end
+//         end
+//         (WRITE_LEVEL2 || READ_LEVEL2) : begin
+//               if(localmemoryAddress[19:16] == 4'hF) begin
+//                     localMemoryAddress[19:16] <= 4'h0;
+//                 end
+//                 else begin
+//                     localMemoryAddress[19:16]++;
+//                 end
+//             end
+//         end
+//         WRITE_LEVEL3 : begin
+//             if(localIoDataOut[3:0] == 4'hF) begin
+//                 localIoDataOut[3:0] <= 4'h0;
+//             end
+//             else begin
+//                 localIoDataOut[3:0]++;
+//             end
+//         end
+//         default : begin
+//         end
+//     endcase
+// end
+
+// always @(posedge sw[1]) begin
+//     case(state)
+//         (WRITE_LEVEL1 || READ_LEVEL1) : begin
+//               if(localmemoryAddress[7:4] == 4'hF) begin
+//                     localMemoryAddress[7:4] <= 4'h0;
+//                 end
+//                 else begin
+//                     localMemoryAddress[7:4]++;
+//                 end
+//             end
+//         end
+//         (WRITE_LEVEL2 || READ_LEVEL2) : begin
+//               if(localmemoryAddress[23:20] == 4'hF) begin
+//                     localMemoryAddress[23:20] <= 4'h0;
+//                 end
+//                 else begin
+//                     localMemoryAddress[23:20]++;
+//                 end
+//             end
+//         end
+//         WRITE_LEVEL3 : begin
+//             if(localIoDataOut[7:4] == 4'hF) begin
+//                 localIoDataOut[7:4] <= 4'h0;
+//             end
+//             else begin
+//                 localIoDataOut[7:4]++;
+//             end
+//         end
+//         default : begin
+//         end
+//     endcase
+// end
+
+// always @(posedge sw[2]) begin
+//     case(state)
+//         (WRITE_LEVEL1 || READ_LEVEL1) : begin
+//               if(localmemoryAddress[11:8] == 4'hF) begin
+//                     localMemoryAddress[11:8] <= 4'h0;
+//                 end
+//                 else begin
+//                     localMemoryAddress[11:8]++;
+//                 end
+//             end
+//         end
+//         (WRITE_LEVEL2 || READ_LEVEL2) : begin
+//               if(localmemoryAddress[24] == 1'b1) begin
+//                     localMemoryAddress[24] <= 1'b0;
+//                 end
+//                 else begin
+//                     localMemoryAddress[24]++;
+//                 end
+//             end
+//         end
+//         WRITE_LEVEL3 : begin
+//             if(localIoDataOut[11:8] == 4'hF) begin
+//                 localIoDataOut[11:8] <= 4'h0;
+//             end
+//             else begin
+//                 localIoDataOut[11:8]++;
+//             end
+//         end
+//         default : begin
+//         end
+//     endcase
+// end
+
+// always @(posedge sw[3]) begin
+//     case(state)
+//         (WRITE_LEVEL1 || READ_LEVEL1) : begin
+//               if(localmemoryAddress[15:12] == 4'hF) begin
+//                     localMemoryAddress[15:12] <= 4'h0;
+//                 end
+//                 else begin
+//                     localMemoryAddress[15:12]++;
+//                 end
+//             end
+//         end
+//         WRITE_LEVEL3 : begin
+//             if(localIoDataOut[15:12] == 4'hF) begin
+//                 localIoDataOut[15:12] <= 4'h0;
+//             end
+//             else begin
+//                 localIoDataOut[15:12]++;
+//             end
+//         end
+//         default : begin
+//         end
+//     endcase
+// end
