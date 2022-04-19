021a600dcf9b9340d8a701068cf206fd4fb839a5
Will-McCormick <wrm0006@uah.edu>
2022-04-19 14:26:11 -0500

    Added Write Command Testbench

--- src/InOutControl_tb.sv
+++ src/InOutControl_tb.sv
@@ -17,12 +17,48 @@ module InOutControl_tb;
     logic [15:0] displayData;
     logic [1:0] mode_output;
 
-    logic [11:0] out_state;
+    logic [12:0] out_state;
 
     //     #10 sw <= 4'b1000;
     //     #10 sw <= 4'b0000;
 
 //Test For READ ********************************
+    // initial begin
+    //     clk <= 1'b0;
+    //     memDone <= 1'b1;
+
+    //     #5 key0_pulse <= 1'b1; key1_pulse <= 1'b1; //Go to IDLE
+    //     #5 key0_pulse <= 1'b0; key1_pulse <= 1'b0;
+
+    //     #5 key0_pulse <= 1'b1; //Go to READ_ST0
+    //     #5 key0_pulse <= 1'b0;
+
+    //     #10 sw <= 9'b011111111; //Set memoryAddress [7:0] to all 1's at next state transition
+
+    //     #5 key1_pulse <= 1'b1; //Go to READ_ST1
+    //     #5 key1_pulse <= 1'b0;
+
+    //   //#10 sw <= 9'b000000000
+    //     #10 sw <= 9'b011111111; //Set memoryAddress [15:8] to all 1's at next state transition
+
+    //     #5 key1_pulse <= 1'b1; //Go to READ_ST2
+    //     #5 key1_pulse <= 1'b0;
+
+    //     #10 sw <= 9'b111111111; //Set memoryAddress [24:16] to all 1's at next state transition
+
+    //     #5 key1_pulse <= 1'b1; memDone<=1'b0; //Go to READ_WAIT
+    //     #5 key1_pulse <= 1'b0;
+
+    //     #5 read_data <= 16'hAAAA;
+    //     #5 memDone <= 1'b1; //Go to READ_DONE
+
+    //     #5 key0_pulse <= 1'b1; //Go to IDLE
+    //     #5 key0_pulse <= 1'b0;
+    // end
+
+//Test For READ ********************************
+
+//Test For WRITE ********************************
     initial begin
         clk <= 1'b0;
         memDone <= 1'b1;
@@ -30,33 +66,41 @@ module InOutControl_tb;
         #5 key0_pulse <= 1'b1; key1_pulse <= 1'b1; //Go to IDLE
         #5 key0_pulse <= 1'b0; key1_pulse <= 1'b0;
 
-        #5 key0_pulse <= 1'b1; //Go to READ_ST0
+        #5 key0_pulse <= 1'b1; //WRITE_ST0
         #5 key0_pulse <= 1'b0;
 
         #10 sw <= 9'b011111111; //Set memoryAddress [7:0] to all 1's at next state transition
 
-        #5 key1_pulse <= 1'b1; //Go to READ_ST1
+        #5 key1_pulse <= 1'b1; //Go to WRITE_ST1
         #5 key1_pulse <= 1'b0;
 
       //#10 sw <= 9'b000000000
         #10 sw <= 9'b011111111; //Set memoryAddress [15:8] to all 1's at next state transition
 
-        #5 key1_pulse <= 1'b1; //Go to READ_ST2
+        #5 key1_pulse <= 1'b1; //Go to WRITE_ST2
         #5 key1_pulse <= 1'b0;
 
         #10 sw <= 9'b111111111; //Set memoryAddress [24:16] to all 1's at next state transition
 
-        #5 key1_pulse <= 1'b1; memDone<=1'b0; //Go to READ_WAIT
+        #5 key1_pulse <= 1'b1; //Go to WRITE_ST3
         #5 key1_pulse <= 1'b0;
 
-        #5 read_data <= 16'hAAAA;
-        #5 memDone <= 1'b1; //Go to READ_DONE
+        #10 sw<= 9 'b011001100; //Set write_data [7:0] to all 1's at next state transition
 
-        #5 key0_pulse <= 1'b1; //Go to IDLE
-        #5 key0_pulse <= 1'b0;
+        #5 key1_pulse <= 1'b1; //Go to WRITE_ST4
+        #5 key1_pulse <= 1'b0;
+
+        #10 sw <= 9'b011001100; //Set write_data [15:0] to all 1's at next state transition
+
+        #5 key1_pulse <= 1'b1; memDone<=1'b0; //Go to WRITE_WAIT
+        #5 key1_pulse <= 1'b0;
+
+
+        #5 memDone <= 1'b1; //Go to IDLE
+
     end
 
-//Test For READ ********************************
+//Test For WRITE ********************************
 
     always begin
         #delay clk=~clk;
